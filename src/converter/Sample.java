3b29b78cf643ea5ab1f4219dcb79b4ca3bce94e6
sven <sven@sven-buero.fritz.box>
2012-07-13 12:36:59 +0200

    Ausarbeitung.pdf updated

--- src/converter/Sample.java
+++ src/converter/Sample.java
@@ -6,7 +6,7 @@ import javax.xml.bind.JAXBException;
 public class Sample {
 
 	public void convertFromTigerXmlToFramenet() {
-		// Step 1: Create a Java Object from a corpus file 
+		// Step 1: Create a Java Object from a corpus file.
 		tigerxml.Corpus tigerCorpus = null;
 		try {
 			tigerxml.TigerXmlMapper.load("<Path to TigerXML corpus file>");
@@ -17,14 +17,14 @@ public class Sample {
 		}
 		//	You may make some changes to the corpus object here.
 		
-		// Step 2: Create a converter from the source corpus object
+		// Step 2: Create a converter from the source corpus object.
 		converter.TigerXmlConverter converter = new converter.TigerXmlConverter(tigerCorpus);
 		
-		// Step 3: Export it to the source format (FrameNet here)
+		// Step 3: Export it to the source format (FrameNet here).
 		framenet.corpus.Corpus framenetCorpus = converter.toFrameNet("<corpus name>");
-		//	Additional changes to the FrameNet corpus object can be made
+		//	Additional changes to the FrameNet corpus object can be made.
 		
-		// Step 4: Save the object to a file
+		// Step 4: Save the object to a file.
 		try {
 			framenet.FrameNetMapper.save(framenetCorpus, "<Path to FrameNet corpus file>");
 		} catch (IOException e) {
